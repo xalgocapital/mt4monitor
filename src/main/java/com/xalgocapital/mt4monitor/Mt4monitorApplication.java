d38749392bf20819c2b7dec5f4cb3610314fe6c5
zhangtao <zhangtaolv@139.com>
2024-02-17 09:03:22 +0800

    submit code

---
+++ src/main/java/com/xalgocapital/mt4monitor/Mt4monitorApplication.java
@@ -0,0 +1,13 @@
+package com.xalgocapital.mt4monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mt4monitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mt4monitorApplication.class, args);
+	}
+
+}
